d48fe8c192b1d5c716fd962b7784300403931f46
Joe Zeng <joe@monsteragames.com>
2016-03-10 01:27:17 -0500

    All the attacks from the original game are in!

--- js/attacks/attacks.js
+++ js/attacks/attacks.js
@@ -13,11 +13,16 @@ var attacks = {
 	ag10: ag10,
 	sp2: sp2,
 	ag11: ag11,
-
-
+	ag12: ag12,
+	ag13: ag13,
+	ag14: ag14,
+	ag15: ag15,
+	ag16: ag16,
+	ag17: ag17,
+	ag18: ag18,
 
 	g_ag1: g_ag1,
 	g_sp1: g_sp1,
 	g_sp2: g_sp2,
-	
+
 };
--- js/attacks/normal.js
+++ js/attacks/normal.js
@@ -233,7 +233,146 @@ var ag11 = {
 		{ direction: "2", turntype: 0, target_time: 0.4, speed: 200 },
 		{ direction: "4", turntype: 0, target_time: 0.8, speed: 200 },
 		{ direction: "2", turntype: 0, target_time: 1.2, speed: 200 },
-		{ direction: "4", turntype: 2, target_time: 1.6, speed: 300 },
+		{ direction: "4", turntype: 3, target_time: 1.6, speed: 300 },
+	],
+	next_sets: ["ag12"],
+};
+
+var ag12 = {
+	type: "arrow",
+	id: "ag11",
+	next_time: 4.4,
+	arrows: [
+		{ direction: "2", turntype: 0, target_time: 0.0, speed: 200 },
+		{ direction: "4", turntype: 0, target_time: 0.4, speed: 200 },
+		{ direction: "1", turntype: 2, target_time: 0.8, speed: 200 },
+		{ direction: "1", turntype: 0, target_time: 1.2, speed: 200 },
+		{ direction: "2", turntype: 0, target_time: 1.6, speed: 300 },
+		{ direction: "4", turntype: 0, target_time: 2.0, speed: 300 },
+		{ direction: "3", turntype: 0, target_time: 2.4, speed: 300 },
+		{ direction: "1", turntype: 3, target_time: 2.8, speed: 300 },
+	],
+	next_sets: ["ag13"],
+};
+
+
+var ag13 = {
+	type: "arrow",
+	id: "ag13",
+	next_time: 4.4,
+	arrows: [
+		{ direction: "2", turntype: 0, target_time: 0.0, speed: 200 },
+		{ direction: "1", turntype: 0, target_time: 0.4, speed: 200 },
+		{ direction: "4", turntype: 0, target_time: 0.8, speed: 200 },
+		{ direction: "3", turntype: 0, target_time: 1.2, speed: 200 },
+		{ direction: "4", turntype: 2, target_time: 2.0, speed: 300 },
+		{ direction: "3", turntype: 3, target_time: 2.4, speed: 300 },
+		{ direction: "2", turntype: 2, target_time: 2.8, speed: 300 },
+		{ direction: "1", turntype: 3, target_time: 3.2, speed: 300 },
+	],
+	next_sets: ["ag14"],
+};
+
+
+var ag14 = {
+	type: "arrow",
+	id: "ag14",
+	next_time: 4.8,
+	arrows: [
+		{ direction: "4", turntype: 0, target_time: 0.0, speed: 200 },
+		{ direction: "4", turntype: 0, target_time: 0.35, speed: 200 },
+		{ direction: "2", turntype: 0, target_time: 0.7, speed: 200 },
+		{ direction: "2", turntype: 0, target_time: 1.05, speed: 200 },
+		{ direction: "1", turntype: 0, target_time: 1.4, speed: 300 },
+		{ direction: "3", turntype: 2, target_time: 1.75, speed: 300 },
+		{ direction: "3", turntype: 0, target_time: 2.1, speed: 300 },
+		{ direction: "1", turntype: 2, target_time: 2.45, speed: 300 },
+		{ direction: "1", turntype: 0, target_time: 2.8, speed: 500 },
+		{ direction: "4", turntype: 0, target_time: 3.3, speed: 500 },
+		{ direction: "2", turntype: 0, target_time: 3.6, speed: 500 },
+	],
+	next_sets: ["ag15"],
+};
+
+var ag15 = {
+	type: "arrow",
+	id: "ag15",
+	next_time: 4.7,
+	arrows: [
+		{ direction: "4", turntype: 0, target_time: 0.0, speed: 200 },
+		{ direction: "3", turntype: 0, target_time: 0.35, speed: 200 },
+		{ direction: "3", turntype: 0, target_time: 0.7, speed: 200 },
+		{ direction: "3", turntype: 2, target_time: 1.6, speed: 200 },
+		{ direction: "2", turntype: 0, target_time: 1.8, speed: 300 },
+		{ direction: "3", turntype: 0, target_time: 2.6, speed: 300 },
+		{ direction: "2", turntype: 0, target_time: 2.9, speed: 300 },
+		{ direction: "4", turntype: 3, target_time: 3.2, speed: 300 },
+		{ direction: "2", turntype: 0, target_time: 3.5, speed: 300 },
+	],
+	next_sets: ["ag16"],
+};
+
+
+var ag16 = {
+	type: "arrow",
+	id: "ag16",
+	next_time: 4.6,
+	arrows: [
+		{ direction: "3", turntype: 0, target_time: 0.0, speed: 500 },
+		{ direction: "4", turntype: 0, target_time: 0.4, speed: 500 },
+		{ direction: "4", turntype: 0, target_time: 0.8, speed: 500 },
+		{ direction: "1", turntype: 0, target_time: 1.2, speed: 500 },
+		{ direction: "1", turntype: 0, target_time: 1.6, speed: 500 },
+		{ direction: "4", turntype: 0, target_time: 2.0, speed: 500 },
+		{ direction: "3", turntype: 0, target_time: 2.4, speed: 500 },
+		{ direction: "4", turntype: 0, target_time: 2.8, speed: 500 },
+		{ direction: "4", turntype: 0, target_time: 3.2, speed: 500 },
+		{ direction: "3", turntype: 0, target_time: 3.6, speed: 500 },
+	],
+	next_sets: ["ag17"],
+};
+
+
+var ag17 = {
+	type: "arrow",
+	id: "ag17",
+	next_time: 4.5,
+	arrows: [
+		{ direction: "2", turntype: 0, target_time: 0.0, speed: 300 },
+		{ direction: "2", turntype: 0, target_time: 0.32, speed: 300 },
+		{ direction: "2", turntype: 0, target_time: 0.64, speed: 300 },
+		{ direction: "2", turntype: 0, target_time: 0.80, speed: 300 },
+		{ direction: "2", turntype: 0, target_time: 0.96, speed: 300 },
+		{ direction: "2", turntype: 0, target_time: 1.28, speed: 300 },
+		{ direction: "4", turntype: 3, target_time: 1.60, speed: 300 },
+		{ direction: "2", turntype: 0, target_time: 1.92, speed: 300 },
+		{ direction: "4", turntype: 3, target_time: 2.24, speed: 300 },
+		{ direction: "4", turntype: 0, target_time: 2.56, speed: 300 },
+		{ direction: "2", turntype: 2, target_time: 2.88, speed: 300 },
+		{ direction: "4", turntype: 0, target_time: 3.20, speed: 300 },
+		{ direction: "2", turntype: 2, target_time: 3.52, speed: 300 },
+	],
+	next_sets: ["ag18"],
+};
+
+
+var ag18 = {
+	type: "arrow",
+	id: "ag18",
+	next_time: 4.5,
+	arrows: [
+		{ direction: "4", turntype: 3, target_time: 0.0, speed: 300 },
+		{ direction: "3", turntype: 2, target_time: 0.35, speed: 300 },
+		{ direction: "2", turntype: 2, target_time: 0.7, speed: 300 },
+		{ direction: "1", turntype: 2, target_time: 1.05, speed: 300 },
+		{ direction: "4", turntype: 3, target_time: 1.4, speed: 300 },
+		{ direction: "3", turntype: 2, target_time: 1.75, speed: 300 },
+		{ direction: "2", turntype: 2, target_time: 2.1, speed: 300 },
+		{ direction: "1", turntype: 2, target_time: 2.45, speed: 300 },
+		{ direction: "4", turntype: 3, target_time: 2.8, speed: 300 },
+		{ direction: "3", turntype: 2, target_time: 3.15, speed: 300 },
+		{ direction: "2", turntype: 2, target_time: 3.5, speed: 300 },
+		{ direction: "1", turntype: 2, target_time: 3.85, speed: 300 },
 	],
 	next_sets: ["sp2"],
 };
